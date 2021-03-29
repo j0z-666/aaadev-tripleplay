403d38f5233e9a3a3bde448b946d0dc3238cabf7
martinez71 <j_alberto_v1@hotmail.com>
2021-03-29 09:11:37 -0600

    inicial

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const axios = require('axios');
+var exphbs  = require('express-handlebars');
+const http = require('http');
+
+var app = express();
+server = http.createServer(app);
+
+app.use(express.static('public'));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/citizenInfo', function(req, res) {
+
+    if(req.query.id == undefined){
+        res.send('Bad Request');
+        console.log('El request no tiene id');
+        return;
+    }
+    console.log("app get /citizenInfo, query id = " + req.query.id);
+
+    axios.get('https://breeze2-196.collaboratory.avaya.com/services/AAADEVContextStore/cs/contexts/'+req.query.id)
+    .then(function(resp){
+        if(resp.data.data != undefined && resp.data.data.valid != undefined){
+            let datos = resp.data.data;
+            datos.jsonStr = JSON.stringify(datos, undefined, 4);
+            res.render('ContextView', datos);
+        } else {
+            res.render('ContextView', {});
+        }
+    })
+    .catch(function(error){
+        console.log('Axios error: ' + error);
+        res.render('ContextView', {});
+    })
+});
+
+app.get('/makepayent', function(req, res){
+    return axios.get('')
+    .then(function(resp){
+        return axios.get('')
+        .then(function(resp1){
+            res.render('MakePayment');
+
+        })
+        .catch(function(error1){
+
+        });
+    })
+    .catch(function(error){
+
+    });
+});
+
+
+var puertoApp = process.env.PORT || 8080;
+server.listen(puertoApp, () => {
+
+  console.log('Corriendo Server puerto ' + puertoApp);
+
+});
+
+module.exports = app;
